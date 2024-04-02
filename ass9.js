cd11e8029c82f9509fcd49797c7974b6a24f63a3
RestorerBarisereden <restorerzorzor@gmail.com>
2024-04-02 23:44:31 +0100

    first push

---
+++ ass9.js
@@ -0,0 +1,10 @@
+let temp = 37
+if (temp < 0){
+    console.log("Warning potential freezing condition");
+}
+else if (temp > 35){
+    console.log("Warning against heatwave");
+}
+else {
+    console.log("Temperature is in normal condition");
+}
